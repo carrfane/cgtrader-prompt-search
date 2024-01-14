836b012566f6a2cd7b924d1577d15931161a1cb7
Hector Carrillo <hector.carrillo@go1.com>
2024-01-14 00:16:27 -0300

    add turbo frame for reactivity

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,10 @@
 class SearchController < ApplicationController
   before_action :search_terms
 
-  def search
-    render partial: "results"
-  end
-
   private
 
   def search_terms
-    @search_term = params[:q]
-    @results = Prompt.search(@search_term, fields: [:prompt], limit: 10)
+    @search = params[:search]
+    @results = Prompt.search(@search, fields: [:prompt], limit: 10)
   end
 end
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,15 +1,15 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['search', 'results', 'result']
+  static targets = ['search']
 
-  async fetch() {
-    event.preventDefault();
-    const search_term = this.searchTarget.value;
-    const response = await fetch(`/search?q=${search_term}`);
-    const responseHTML = await response.text();
-    this.resultsTarget.innerHTML = responseHTML;
-    window.history.pushState({}, '', `/?q=${search_term}`);
+  connect() {
+    try {
+      const search_term = this.searchTarget.innerText.trim();
+      window.history.pushState({}, '', `/?search=${search_term}`)
+    } catch {
+      return;
+    }
   }
 
   async copy(event) {
